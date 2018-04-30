5ecd1595522e7851253095fd2d0c534eea3316b7
Haoyuan Liu <15911084003@163.com>
2018-04-29 21:13:30 -0500

    App

--- app/src/main/java/com/example/mac/stellarwalker/MainActivity.java
+++ app/src/main/java/com/example/mac/stellarwalker/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.mac.stellarwalker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intent);
     }
 }
